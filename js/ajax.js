bc0b070952522befd0a17f7901c2c3aeaf5ff4e1
2016214495 <deng1063492518@qq.com>
2017-01-31 16:47:30 +0800

    注册

---
+++ js/ajax.js
@@ -0,0 +1,46 @@
+function ajax(opts) {
+    var defaults = {
+        method:'GET',
+        url:'',
+        data:'',
+        async:true,
+        cache:true,
+        contentType:'application/x-www-form-urlencoded',
+        success:function() {},
+        error:function() {},
+    };
+    for(var key in opts) {
+        defaults[key] = opts[key];
+    };
+    if (typeof defaults,data === 'object') {
+        var str = '';
+        for(var key in defaults.data) {
+            str += key + '=' + defaults.data[key] + '&'
+        }
+        defaults.data = str.substring(0,str.length - 1);
+    };
+    defaults.method = defaults.method.toUpperCase();
+    defaults.cache = defaults.cache ? '' : '&' + new Date().getTime();
+    if (defaults.method === 'GET' && (defaults.data || defaults.cache) ) {
+        defaults.url += '?' + defaults.data + defaults.cache;
+    };
+    var oXhr = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject('Microsoft.XMLHTTP');
+    oXhr.open(defaults.method,defaults.url,defaults.asycn);
+    if (defaults.method === 'GET') {
+        oXhr.send(null);
+    }
+    else {
+        oXhr.setReruestHeader("Content-type",defaults.contentType);
+        oXhr.send(defaults.data);
+    }
+    oXhr.onreadystatechange = function () {
+        if (oXhr.readyState === 4) {
+            if (oXhr.status === 200) {
+                defaults.success.call(oXhr,oXhr.responseText);
+            }
+            else {
+                defaults.error();
+            };
+        };
+    };
+};
--- js/enroll.js
+++ js/enroll.js
@@ -40,7 +40,7 @@ xmlhttp.onreadystatechange = function ()
     }
 }
 xmlhttp.send("usename="+yourname+"&pwd="+yourpwd);*/
-document.querySelector('#myBtn').addEventListener("click",function() {
+/*document.querySelector('#myBtn').addEventListener("click",function() {
     var username = document.querySelector('#username');
     username = username.value;
     if (username.trim() === ''||username == null) {
@@ -61,7 +61,7 @@ document.querySelector('#myBtn').addEventListener("click",function() {
             document.querySelector('#rig-nc').innerHTML = "可以使用";
         }
     }
-    var password = document.querySelector('#password');
+    var pwd = document.querySelector('#yourpwd');
     password = password.value;
     if (password.length < 6) {
         document.querySelector('#rig-mm').style.color = "red";
@@ -105,4 +105,179 @@ document.querySelector('#myBtn').addEventListener("click",function() {
             }
         }
     }
+});*/
+
+/*document.querySelector("#yourpwd").addEventListener("mouseup",function() {
+    var username = document.querySelector('#yourname');
+    username = username.value;
+    if (username.trim() === ''||username == null) {
+       document.querySelector("#rig-nc").style.color = "red";
+       document.querySelector("#rig-nc").innerHTML = "请设置昵称"; 
+    }
+    else {
+        if (username == "forbid") {
+            document.querySelector('#rig-nc').style.color = "red";
+            document.querySelector('#rig-nc').innerHTML = "含有非法词语";
+        }
+        else if (username == "already have") {
+            document.querySelector('#rig-nc').style.color = "red";
+            document.querySelector('#rig-nc').innerHTML = "用户名已被使用";
+        }
+        else {
+            document.querySelector('#rig-nc').style.color = "green";
+            document.querySelector('#rig-nc').innerHTML = "可以使用";
+        }
+    }
 });
+document.querySelector("#postbox").addEventListener("mouseup",function() {
+    var pwd = document.querySelector('#yourpwd');
+    pwd = pwd.value;
+    if (pwd.length < 6||pwd.length >20) {
+        document.querySelector('#rig-mm').style.color = "red";
+        document.querySelector('#rig-mm').innerHTML = "密码长度不符合要求";
+    }
+    else {
+        if(pwd.length >= 6 && pwd.length < 10) {
+            document.querySelector('#rig-mm').style.color = "orange";
+            document.querySelector('#rig-mm').innerHTML = "密码级别：低";
+        }
+        else if(pwd.length >= 10 && pwd.length < 15) {
+            document.querySelector('#rig-mm').style.color = "yellow";
+            document.querySelector('#rig-mm').innerHTML = "密码级别：中";
+        }
+        else {
+            document.querySelector('#rig-mm').style.color = "green";
+            document.querySelector('#rig-mm').innerHTML = "密码级别：高";
+        }
+    }
+});
+document.querySelector("#code").addEventListener("mouseup",function() {
+     var postbox = document.querySelector('#postbox');
+    postbox = postbox.value;
+    if (postbox == ''||postbox == null) {
+        document.querySelector('#rig-yx').style.color = "red";
+        document.querySelector('#rig-yx').innerHTML = "请设置邮箱";
+    }
+    else {
+        for (var i = 0; i < postbox.length; i++) {
+            var x = "@" ;
+            if (postbox[i] == x ) {
+                document.querySelector('#rig-yx').style.color = "green";
+                document.querySelector('#rig-yx').innerHTML = "记住邮箱";
+                break;
+            }
+            else {
+                document.querySelector('#rig-yx').style.color = "red";
+                document.querySelector('#rig-yx').innerHTML = "请输入正确的邮箱地址";
+            }
+        }
+    }
+});*/
+/*function check() {
+    var username = document.querySelector("#yourname").value;
+    if (username == "" || username == null) {
+        document.querySelector("#rig-nc").style.color = "red";
+        document.querySelector("#rig-nc").innerHTML = "用户名为6-20位数字、字母或符号";
+    }
+    else {
+        ajax("../php/signup.php",function(responseText) {
+            if (responseText == "forbid") {
+                document.querySelector("#rig-nc").style.color = "red";
+                document.querySelector("#rig-nc").innerHTML = "用户名含有非法词语";
+            }
+            else if (responseText == "already have") {
+                document.querySelector("#rig-nc").style.color = "red";
+                document.querySelector("#rig-nc").innerHTML = "用户名已被使用";
+            }
+            else {
+                document.querySelector("#rig-nc").style.color = "green";
+                document.querySelector("#rig-nc").innerHTML = "可以使用";
+            }
+        })
+    }
+}
+function password() {
+    var pwd = document.querySelector("#yourpwd").value;
+    if (pwd == "" || pwd == null) {
+        document.querySelector("#rig-mm").style.color = "red";
+        document.querySelector("#rig-mm").innerHTML = "请设置密码";
+    }
+    else {
+        if (pwd.length < 6 || pwd.length > 20) {
+            document.querySelector("#rig-mm").style.color = "red";
+            document.querySelector("#rig-mm").innerHTML = "密码长度不符合要求";
+        }
+        else {
+            ajax("../php/signup.php");
+        }
+    }
+}
+function second() {
+    var tpwd = document.querySelector("#tpwd").value;
+    if (pwd != tpwd) {
+        document.querySelector("#rig-qrmm").style.color = "red";
+        document.querySelector("rig-qrmm").innerHTML = 
+    }
+}*/
+document.querySelector("#myBtn").addEventListener("click",check);
+function check() {
+    var username = document.querySelector("#yourname").value;
+    if (username == "" || username == null) {
+        document.querySelector("#rig-nc").style.color = "red";
+        document.querySelector("rig-nc").innerHTML = "请设置昵称";
+    }
+    else {
+        ajax("../php/signup.php",function(responseText) {
+            if (responseText == "forbid") {
+                document.querySelector("#rig-nc").style.color = "red";
+                document.querySelector("#rig-nc").innerHTML = "用户名含有非法词语";
+            }
+            else if (responseText == "already have") {
+                document.querySelector("#rig-nc").style.color = "red";
+                document.querySelector("#rig-nc").innerHTML = "用户名已被使用";
+            }
+            else {
+                document.querySelector("#rig-nc").style.color = "green";
+                document.querySelector("#rig-nc").innerHTML = "可以使用";
+            }
+        })
+    }
+    var pwd = document.querySelector("#yourpwd").value;
+    if (username == "" || username == null) {
+        document.querySelector("#rig-mm").style.color = "red";
+        document.querySelector("rig-mm").innerHTML = "请设置密码";
+    }
+    else {
+        if (pwd.length < 6 || pwd.length > 20) {
+            document.querySelector("#rig-mm").style.color = "red";
+            document.querySelector("#rig-mm").innerHTML = "密码长度不符合要求";
+        }
+        else {
+            ajax("../php/signup.php",function(password) {
+                document.querySelector("#rig-mm").style.color = "green";
+                document.querySelector("#rig-mm").innerHTML = "可以使用";
+            })
+        }
+    }
+    var pbx = document.querySelector("#postbox").value;
+    if (postbox == ''||postbox == null) {
+        document.querySelector('#rig-yx').style.color = "red";
+        document.querySelector('#rig-yx').innerHTML = "请设置邮箱";
+    }
+    else {
+        for (var i = 0; i < postbox.length; i++) {
+            var x = "@" ;
+            if (postbox[i] == x ) {
+                ajax("../php/signup.php",function(postbox) {
+                    document.querySelector('#rig-yx').style.color = "green";
+                    document.querySelector('#rig-yx').innerHTML = "记住邮箱";
+                    break;
+                })
+            }
+            else {
+                document.querySelector('#rig-yx').style.color = "red";
+                document.querySelector('#rig-yx').innerHTML = "请输入正确的邮箱地址";
+            }
+        }
+    }
+}
