f6dcc8521f005b3ca4ca2df5bbcbc88a2f035fee
DonghongHan <819400472@qq.com>
2017-02-03 15:23:54 +0800

    meisha

--- php/signup.php
+++ php/signup.php
@@ -1,6 +1,6 @@
 <?php
 // session_start();
-echo 'asdasd';
+
 $username = $_POST['yourname'];
 $password = $_POST['yourpwd'];
 $email = $_POST['postbox'];
@@ -8,41 +8,31 @@ $code = $_POST['code'];
 
 $illegalchar = '~!@#$%^&*()_+{}|:"<>?`-=[]\;\',./';
 
-    $username = addslashes($_POST['yourname']);
-    $salt = "tianwanggaidihu";
-    $password = md5(md5($_POST['yourpwd']).$salt);
-    try 
+$username = addslashes($_POST['yourname']);
+$salt = "tianwanggaidihu";
+$password = md5(md5($_POST['yourpwd']).$salt);
+try 
+{
+    $config = require_once 'config.php';
+    $pdo = new PDO($config['host'],$config['user'],$config['pass']);
+    $sql ='INSERT INTO bilibili_user (username,pwd,email) VALUES("'.$username.'","'.$password.'","'.$email.'")';
+    $res = $pdo->exec($sql);
+    // if (!$res) {
+    //     echo "\nPDO::errorInfo():\n";
+    //     print_r($pdo->errorInfo());
+    // }
+    if($res == 1)
     {
-        $config = require_once 'config.php';
-        $pdo = new PDO($config['host'],$config['user'],$config['pass']);
-        // $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        $result = $pdo->prepare("SELECT * FROM bilibili_user WHERE username=:username");
-        $result->bindparam(":username",$username);
-        $result->execute();
-        $data = $result->fetch(PDO::FETCH_ASSOC);
-            if(!empty($data))
-            {
-                echo 'already exists';
-            }
-            else
-            {
-                $sql ='INSERT INTO bilibili_user (username,pwd,email) VALUES("'.$username.'","'.$password.'","'.$email.'")';
-                $res2 = $pdo->exec($sql);
-                if($res2 == 1)
-                {
-                    echo 'sucess';
-                }
-                // else
-                // {
-                //     print_r($pdo->errorInfo());
-                // }
-            }
+        echo 'sucess';
+    }
+    else{
         
     }
-    catch (PDOException $e) 
-    {
-        echo "数据库错误：".$e->getMessage();
-    }
+}
+catch (PDOException $e) 
+{
+    echo "数据库错误：".$e->getMessage();
+}
 
 
 ?>
