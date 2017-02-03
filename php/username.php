76628afe1315861bbd31ec15624952958a3ff68c
DonghongHan <819400472@qq.com>
2017-02-03 12:33:20 +0800

    里面有地方打错了

--- php/username.php
+++ php/username.php
@@ -7,7 +7,7 @@ $username = addslashes($username);
 $config = require_once('config.php');
 $pdo = new PDO($config['host'],$config['user'],$config['pass']);
 $result = $pdo->prepare("SELECT username FROM bilibili_user where username = :username");
-$result->bindParam(":uaername",$username);
+$result->bindParam(":username",$username);
 $isexists = $result->execute();
 if($isexists){
     echo 'already exists';
