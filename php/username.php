4262b9bdc558e0092fcde0d48cb467e52ab5778f
DonghongHan <819400472@qq.com>
2017-02-03 13:06:00 +0800

    修改用户名总是存在

--- php/username.php
+++ php/username.php
@@ -8,8 +8,9 @@ $config = require_once('config.php');
 $pdo = new PDO($config['host'],$config['user'],$config['pass']);
 $result = $pdo->prepare("SELECT username FROM bilibili_user where username = :username");
 $result->bindParam(":username",$username);
-$isexists = $result->execute();
-if($isexists){
+$result->execute();
+$isexists = $result->fetch(PDO::FETCH_ASSOC);
+if(!empty($isexists)) {
     echo 'already exists';
 }else{
     echo 'can be used';
