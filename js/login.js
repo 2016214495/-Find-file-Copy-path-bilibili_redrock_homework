9706d6a5a09d4b24d4d2d8dd98f31d5be909a3b4
2016214495 <deng1063492518@qq.com>
2017-02-05 18:22:53 +0800

    加入了登录的前端代码

---
+++ js/login.js
@@ -0,0 +1,29 @@
+document.querySelector("#log_login").addEventListener("click",clicker);
+function clicker() {
+    var $yourname = document.querySelector("#yourname").value;
+    var $yourpwd = document.querySelector("#yourpwd").value;
+    if ($yourname == "" || $yourname == null) {
+        document.querySelector("#log_username_ts").style.color = "red";
+        document.querySelector("#log_username_ts").innerHTML = "请输入用户名" ;
+    }
+    if ($yourpwd == "" ||yourpwd == null) {
+        document.querySelector("#log_pwd_ts").style.color = "red";
+        document.querySelector("#log_pwd_ts").innerHTML = "请输入密码";
+    }
+    if ($yourname != "" || $yourpwd != "" || $yourname != null || $yourpwd != null) {
+        ajax({
+            method: 'POST',
+            url: '../php/username.php',
+            data: {
+                yourname: $yourname,
+                yourpwd: $yourpwd,
+            },
+            success: function(responseText) {
+                if (responseText == "fail") {
+                    document.querySelector("#log_pwd_ts").style.color = "red";
+                    document.querySelector("#log_pwd_ts").innerHTML = "用户名或密码错误";
+                }
+            }
+        })
+    }
+}
