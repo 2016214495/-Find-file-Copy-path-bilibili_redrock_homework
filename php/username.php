b16f07a09eacd0e4efd5e92be599f424af7cde05
DonghongHan <819400472@qq.com>
2017-02-07 18:27:05 +0800

    对登录注册的php改为oop

--- php/username.php
+++ php/username.php
@@ -1,18 +1,23 @@
 <?php
+if (!isset($_SESSION)) {
+    session_start();
+}
 
 $username = $_POST['yourname'];
 $username = addslashes($username);
 
 $config = require_once('config.php');
 $pdo = new PDO($config['host'],$config['user'],$config['pass']);
-$result = $pdo->prepare("SELECT username FROM bilibili_user where username = :username");
+$result = $pdo->prepare("SELECT username FROM bilibili_user WHERE username = :username");
 $result->bindParam(":username",$username);
 $result->execute();
 $isexists = $result->fetch(PDO::FETCH_ASSOC);
 if(!empty($isexists)) {
     echo 'already exists';
+    $_SESSION['nameexists'] = true;
 }else{
     echo 'can be used';
+    $_SESSION['nameexists'] = false;
 }
 
 
