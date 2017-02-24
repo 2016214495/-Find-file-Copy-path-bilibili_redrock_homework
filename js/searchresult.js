9c8f25bb892a1cf90da4c194c394b27ce6452253
2016214495 <deng1063492518@qq.com>
2017-02-24 20:13:57 +0800

    搜索结果页面

---
+++ js/searchresult.js
@@ -0,0 +1,48 @@
+function color() {
+    var listdiv = document.querySelectorAll(".listdiv");
+    for (var i = 0; i < listdiv.length; i++) {
+        listdiv[i].addEventListener("mouseover",function() {
+            this.style.color = "#00a1d6";
+        })
+        listdiv[i].addEventListener("mouseout",function() {
+            this.style.color = "#111111";
+        })
+    }
+}
+color();
+function sty() {
+    var listsug = document.querySelectorAll(".listsug");
+    for (var i = 0; i < listsug.length; i++) {
+        listsug[i].addEventListener("mouseover",function() {
+            this.style.color = "#00a1d6";
+        })
+        listsug[i].addEventListener("mouseout",function() {
+            this.style.color = "#111111";
+        })
+    }
+}
+sty();
+function styo() {
+    var listsugg = document.querySelectorAll(".listsugg");
+    for (var i = 0; i < listsugg.length; i++) {
+        listsugg[i].addEventListener("mouseover",function() {
+            this.style.color = "#00a1d6";
+        })
+        listsugg[i].addEventListener("mouseout",function() {
+            this.style.color = "#111111";
+        })
+    }
+}
+styo();
+function styt() {
+    var listsugge = document.querySelectorAll(".listsugge");
+    for (var i = 0; i < listsugge.length; i++) {
+        listsugge[i].addEventListener("mouseover",function() {
+            this.style.color = "#00a1d6";
+        })
+        listsugge[i].addEventListener("mouseout",function() {
+            this.style.color = "#111111";
+        })
+    }
+}
+styt();
